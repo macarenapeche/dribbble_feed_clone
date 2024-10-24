95be09385147937d2c34afbdb874e64e7afca88c
Macarena Peche <mapeciris@gmail.com>
2024-10-24 17:39:52 +0200

    Fix delete button not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  devise_scope :user do  
-     get '/users/sign_out' => 'devise/sessions#destroy'     
+  devise_scope :user do
+     get "/users/sign_out" => "devise/sessions#destroy"
   end
 
   get "home/index"
@@ -20,6 +20,6 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   resources :shots
-  
+
   root "shots#index"
 end
